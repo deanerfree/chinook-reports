7ad7ed00c6813f7b226b4841ef1099ea8b9e7ed7
Kurt Ball <kurtisdball@hotmail.com>
2026-06-02 22:08:38 +0000

    cleanup unused files

--- priv/repo/migrations/20260425193538_add_report_data_to_reports.exs
+++
@@ -1,9 +0,0 @@
-defmodule ChinookReports.Repo.Migrations.AddReportDataToReports do
-  use Ecto.Migration
-
-  def change do
-    alter table(:reports) do
-      add :metadata, :map
-    end
-  end
-end
